9b65e34c94aca357dd3f25ce5c30919c49e4b64c
colcheski <colcheski@gmail.com>
2022-04-04 19:54:22 -0400

    More mobile responsive changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-$(window).on("hashchange", function (e) {
-  window.history.pushState("", document.title, window.location.pathname);
-  // do something...
+$(document).ready(function () {
+  $(window).on("hashchange", function (e) {
+    window.history.pushState("", document.title, window.location.pathname);
+  });
+
+  $("#myModal").on("hidden.bs.modal", function () {
+    $("#navi-toggle").prop("checked", false);
+  });
+
+  $("#myModal").on("shown.bs.modal", function () {
+    $("#navi-toggle").prop("checked", true);
+  });
+
+  $(".mobileNavigation")
+    .off()
+    .on("click", function () {
+      const href = $(this).attr("href");
+      console.log(href);
+      $("html,body").animate(
+        {
+          scrollTop: $(href).offset().top,
+        },
+        100
+      );
+    });
 });
